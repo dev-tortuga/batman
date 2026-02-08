e2a234815c29860961650dd422eca2a9b082b81d
dev-tortuga <0307wlstjd@gmail.com>
2026-02-08 22:22:00 +0900

    add healthcheck func and requirement.txt
    batman: are you okay?

--- main.py
+++ main.py
@@ -22,7 +22,23 @@ import requests
 import os
 from dotenv import load_dotenv
 
+import aiohttp
+from aiohttp import web
+
+
 #initialize
+
+# 헬스체크
+async def health_check(request):
+  return web.Response(text="OK", status=200)
+
+async def start_web_server():
+  app = web.Application()
+  app.router.add_get('/health', health_check) # Health Check API 추가
+  runner = web.AppRunner(app)
+  await runner.setup()
+  site = web.TCPSite(runner, '0.0.0.0', 8000)
+  await site.start()
 
 # 환경변수 호출 및 토큰 등록
 load_dotenv()
